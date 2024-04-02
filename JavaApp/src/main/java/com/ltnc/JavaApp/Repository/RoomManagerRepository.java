81bba4e25097b1ee9479abc87152d6fadfe50f63
baothai123789 <130385743+baothai123789@users.noreply.github.com>
2024-04-02 13:42:03 +0700

    Delete JavaApp directory

--- JavaApp/src/main/java/com/ltnc/JavaApp/Repository/RoomManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltnc.JavaApp.Repository;
-
-import com.ltnc.JavaApp.Model.RoomManager;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface RoomManagerRepository extends MongoRepository<RoomManager,String> {
-}
