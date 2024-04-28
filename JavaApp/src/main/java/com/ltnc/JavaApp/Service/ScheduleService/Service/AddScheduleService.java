ac36aad426ac9500fa116ed17112409633b0d2db
baothai123789 <banbao22042004@gmail.com>
2024-04-28 15:18:40 +0700

    fix schedule service

--- JavaApp/src/main/java/com/ltnc/JavaApp/Service/ScheduleService/Service/AddScheduleService.java
+++ JavaApp/src/main/java/com/ltnc/JavaApp/Service/ScheduleService/Service/AddScheduleService.java
@@ -4,13 +4,16 @@ import com.ltnc.JavaApp.Model.Schedule;
 import com.ltnc.JavaApp.Repository.ScheduleRepository;
 import com.ltnc.JavaApp.Service.ScheduleService.Interface.IAddScheduleService;
 import com.ltnc.JavaApp.Service.ScheduleService.Interface.ScheduleModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class AddScheduleService<T extends ScheduleModel> implements IAddScheduleService{
-    protected ScheduleRepository scheduleRepository;
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
     @Override
     public void addSchedule(ScheduleModel scheduleModel, Schedule newSchedule) throws NullPointerException{
         scheduleModel.addSchedule(newSchedule);
